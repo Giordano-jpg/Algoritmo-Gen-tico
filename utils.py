8b779155b755aebba75bc3f5a5fca049eafa7ec1
Giordano-jpg <giordano.fcg@gmail.com>
2024-12-09 10:51:00 +0100

    Formula euclidiana y obtención de la mejor ruta

--- utils.py
+++ utils.py
@@ -1,3 +1,6 @@
+import numpy as np
+
+# Leer el archivo y guardar el ID y las coordenadas en la variable Ciudads
 def leer_tsp(ruta_archivo):
     with open(ruta_archivo, 'r') as archivo:
         lines = archivo.readlines()
@@ -24,10 +27,28 @@ def leer_tsp(ruta_archivo):
 
     return coords
 
-# El objetivo de este código es poder utilizarlo en otros archivos
-# Ejemplo de uso
-#ruta = "qa194.tsp"
-#coordenadas = leer_tsp(ruta)
+# Nombre del archivo con las coordenadas:
+ruta = "qa194.tsp"
+coordenadas = leer_tsp(ruta) # Leer archivo
 
-#for ciudad in coordenadas:
-    #print(ciudad)
+# Imprimir lista dentro de "coordenadas"
+for ciudad in coordenadas:
+    print(ciudad)
+
+# Formula euclidiana (Para calcular la distancia entre dos puntos)
+def distancia_euclidiana_np(p1, p2):
+    return np.linalg.norm(np.array(p2) - np.array(p1))
+
+# Lista vacía para su posterior uso
+distancia_calculada = []
+
+# Calcular la distancia de cada coordenada y guardarla en una lista.
+for i in range(len(coordenadas)-1):
+    distancia_calculada.append(distancia_euclidiana_np(coordenadas[i], coordenadas[i+1]))
+
+# Imprimir valores calculados
+for i in distancia_calculada:
+    print(i)
+
+# Obtener el recorrido más óptimo
+print("La distancia más corta es:", min(distancia_calculada))
